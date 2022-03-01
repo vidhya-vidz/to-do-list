b6d2fa28f1fb9fd925fd34dcbe777252135ace89
vidhya-vidz <92030036+vidhya-vidz@users.noreply.github.com>
2022-03-01 15:45:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let addToDoButton = document.getElementById('addToDo');
+let ToDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+	var paragraph = document.createElement('p')
+	paragraph.classList.add('paragraph-styling');
+	paragraph.innerText = inputField.value;
+	ToDoContainer.appendChild(paragraph);
+	inputField.value ="";
+	paragraph.addEventListener('click', function(){
+		paragraph.style.textDecoration = "line-through";
+	})
+	paragraph.addEventListener('dblclick', function(){
+		ToDoContainer.removeChild(paragraph);
+	})
+
+})
